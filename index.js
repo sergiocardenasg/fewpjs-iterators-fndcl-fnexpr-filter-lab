aaf53a693eb8d99fec0130fd26f6531894735fda
sergiocardenasg <sergiocardenas@utexas.edu>
2021-07-05 19:03:32 -0500

    commit tree c7a90c27d0b3c7770bc472072d194d0ad176f522

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(arr, args){
+    return arr.filter(possibleMatch => possibleMatch.toLowerCase() === args.toLowerCase()
+)};
+
+function fuzzyMatch(arr, args){
+    return arr.filter(possibleMatch => possibleMatch.charAt(0).toLowerCase() === args.charAt(0).toLowerCase()
+)};
+
+function matchName(arr, args){
+    return arr.filter(possibleMatch => possibleMatch.name === args
+)};
